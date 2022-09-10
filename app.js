07bc8306aa07dbae627d2b777c331572d1da347b
Mazzaleen <8mzln8@gmail.com>
2022-09-10 17:54:44 -0400

    adfs
    a

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const apiRequest = async () => {
    */
 
   // TODO fill in your own port number 
-  const PORT_NUMBER = "";
+  const PORT_NUMBER = "8010";
 
   const baseUrl = `http://localhost:${PORT_NUMBER}/proxy/api/`
 
@@ -29,7 +29,7 @@ const apiRequest = async () => {
     }
   });
 
-  // console.log(response);
+  console.log(response);
 
   // Return the response in JSON format
   return response.json();
@@ -40,14 +40,31 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
+  
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15", 
+  const filtered_fruits = fruitsArray.filter((fruit) => fruit.nutritions.sugar>15);
+  console.log("sugar below 15", filtered_fruits);
+  const sugarFruitsNames = filtered_fruits.map(fruit => fruit.name);
+  console.log(sugarFruitsNames);
 
   // TODO: Create a new HTML element to display your data 
-
   // TODO: Append your new element to the page
+
+    // TODO: Create a new HTML element to display your data 
+  const newElem = document.createElement('div');
+  newElem.innerHTML = "<p id='beginPara'> Fruits with more than 15g sugar: </p> <br> <br>";
+  // Append the new element to an existing part of the webpage
+  gallery.append(newElem)
+
+  for (let i = 0; i < sugarFruitsNames.length; i++) {
+    const newElement = document.createElement('div');
+    newElement.className = 'sugar-fruits';
+    newElement.innerHTML = sugarFruitsNames[i];
+    gallery.append(newElement);
+  }
 
 }
 
